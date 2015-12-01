652b0c9924f9a1dd5a4e7ae8bc56e1baabd5c6d7
James Benner <james.benner@gmail.com>
2015-12-01 09:21:57 -0500

    Updated Exlixir and added Gravatar support to vCards.

--- resources/views/vcard/show.blade.php
+++ resources/views/vcard/show.blade.php
@@ -2,10 +2,11 @@
 
 @section('content')
 
-<div class="jumbotron">
+<div class="jumbotron" vocab="http://microformats.org/wiki/h-card">
     <h1>vCard #{{ $vcard->id }} Yoz!</h1>
     {!! dump($vcard) !!}
     <div class="h-card">
+        <img class="u-photo" src="http://www.gravatar.com/avatar/{{ md5($vcard->email) }}" alt="{{ $vcard->given_name }} {{ $vcard->family_name }}">
         <p style="margin-bottom: 0"><span class="p-family-name">{{ $vcard->family_name }}</span>, <span class="p-given-name">{{ $vcard->given_name }}</span></p>
         <span class="p-job-title">{{ $vcard->title }}</span><br>
         <a class="u-email"  href="mailto:{{ $vcard->email }}">{{ $vcard->email }}</a>
