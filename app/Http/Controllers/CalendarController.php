ba4156c0a737010350034dfaff38b2fbe539e956
Josh Pike <jpike@igcsoftware.com>
2015-12-09 14:06:27 +0000

    set namespace on Jamesez CalendarController

--- app/Http/Controllers/CalendarController.php
+++ app/Http/Controllers/CalendarController.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace vphoenix\Http\Controllers;
 
-use App\Http\Requests;
-use App\Http\Controllers\Controller;
+use vphoenix\Http\Requests;
+use vphoenix\Http\Controllers\Controller;
 use ArrayObject;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
