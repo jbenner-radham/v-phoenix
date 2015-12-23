47bd77657507dc2a2d4ecfba96191b853fa49046
James Benner <james.benner@gmail.com>
2015-12-23 17:40:21 -0500

    Added initial calendar week template partial.

---
+++ resources/views/_includes/calendars/week.blade.php
@@ -0,0 +1,15 @@
+{{-- @link http://www.w3.org/TR/html5/tabular-data.html --}}
+<table aria-describedby="week" role="presentation">
+    <thead>
+        <th scope="col" id="sunday">    Sunday
+        <th scope="col" id="monday">    Monday
+        <th scope="col" id="tuesday">   Tuesday
+        <th scope="col" id="wednesday"> Wednesday
+        <th scope="col" id="thursday">  Thursday
+        <th scope="col" id="friday">    Friday
+        <th scope="col" id="saturday">  Saturday
+    </thead>
+    <tbody>
+        {{-- <td>... --}}
+    </tbody>
+</table>
