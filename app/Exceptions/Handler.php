3a48aac250f59cafb6b244108c130186f8d7b3fa
James Benner <james.benner@gmail.com>
2015-12-27 21:11:58 -0500

    Added Whoops in as the debug error renderer.

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,9 +4,12 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response as HttpResponse;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Whoops\Handler\PrettyPageHandler as WhoopsPrettyPageHandler;
+use Whoops\Run as WhoopsRun;
 
 class Handler extends ExceptionHandler
 {
@@ -46,6 +49,27 @@ class Handler extends ExceptionHandler
             $e = new NotFoundHttpException($e->getMessage(), $e);
         }
 
+        if (config('app.debug')) {
+            return $this->debugRender($e);
+        }
+
         return parent::render($request, $e);
+    }
+
+    /**
+     * @param  \Exception $e
+     * @return \Illuminate\Http\Response
+     */
+    protected function debugRender(Exception $e)
+    {
+        $whoops = new WhoopsRun;
+        $whoops->pushHandler(new WhoopsPrettyPageHandler);
+        $response = $this->convertExceptionToResponse($e);
+
+        return new HttpResponse(
+            $whoops->handleException($e),
+            $response->getStatusCode(),
+            $response->headers
+        );
     }
 }
